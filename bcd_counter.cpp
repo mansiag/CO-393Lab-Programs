6a25ad59c0e3e89f0456c4fdb839d5b70461556f
mansiag <mansi.29ag@gmail.com>
2018-08-25 22:22:20 +0530

    Changed spaces to Tabs

--- bcd_counter.cpp
+++ bcd_counter.cpp
@@ -12,35 +12,35 @@ int Not(int x){
 }
 
 class SRflipflop{
-   
-    private:
-   		int R;
-   		int S;
-   		int Q;
+	
+private:
+		int R;
+		int S;
+		int Q;
 
-    public:
-     
-    	SRflipflop(){
-        	Q = 0;
-        } 
+public:
 
-    	void initQ(int q){
-     		Q = q;
-     	}
+	SRflipflop(){
+	Q = 0;
+} 
 
-   	 	void set(unsigned int r, unsigned int s){
-   	 		R = r;
-   	 		S = s;
-        	calculate();
-   	 	}
+	void initQ(int q){
+		Q = q;
+	}
 
-   	 	unsigned int getQ(){
-   	 		return Q;
-   		}
+		void set(unsigned int r, unsigned int s){
+			R = r;
+			S = s;
+	calculate();
+		}
 
-   	 	void calculate(){
-   	 		Q = S | (Not(R) & Q);
-   	 	}
+		unsigned int getQ(){
+			return Q;
+		}
+
+		void calculate(){
+			Q = S | (Not(R) & Q);
+		}
 
 };
 
@@ -68,67 +68,67 @@ void drawff(){
 }
 
 int main(){
-   
+
 	SRflipflop A, B, C, D;
-   	int gd = DETECT,gm,left=100,top=100,right=200,bottom=200;
-   	initgraph(&gd,&gm,NULL);
-   	int x = 10, y = 300, count = 0, maxx = getmaxx(), ans;
-   	cout<<"Which type of counter do you want?\n1)Upcounter\n2)Downcounter";
-   	cin>>ans;
-  	if(ans==2){
-   		A.initQ(1);
-   	 	D.initQ(1);
-   	}
+	int gd = DETECT,gm,left=100,top=100,right=200,bottom=200;
+	initgraph(&gd,&gm,NULL);
+	int x = 10, y = 300, count = 0, maxx = getmaxx(), ans;
+	cout<<"Which type of counter do you want?\n1)Upcounter\n2)Downcounter";
+	cin>>ans;
+	if(ans==2){
+		A.initQ(1);
+		D.initQ(1);
+	}
 
-   	drawff();
-   	while(1){
-    	for(int i = 0; i < 30; i++){
-    		circle(x, y - i, 0);
-       		delay(10);
-     	}
-    	unsigned int a = A.getQ();
-     	unsigned int b = B.getQ();
-     	unsigned int c = C.getQ();
-     	unsigned int d = D.getQ();
-     	cout<<a<<b<<c<<d<<endl;
+	drawff();
+	while(1){
+	for(int i = 0; i < 30; i++){
+		circle(x, y - i, 0);
+		delay(10);
+ 	}
+	unsigned int a = A.getQ();
+	unsigned int b = B.getQ();
+	unsigned int c = C.getQ();
+	unsigned int d = D.getQ();
+ 	cout<<a<<b<<c<<d<<endl;
 
-     	if(ans == 1){
-     		A.set(Not(c) & d, b & c & d);
-     		B.set(b & c & d, Not(b) & c & d);
-     		C.set(c & d, Not(a) & Not(c) & d);
-     		D.set(d, Not(d));
-    	}
+	if(ans == 1){
+		A.set(Not(c) & d, b & c & d);
+		B.set(b & c & d, Not(b) & c & d);
+		C.set(c & d, Not(a) & Not(c) & d);
+		D.set(d, Not(d));
+	}
 
-    	else{
- 	   		A.set(a & Not(d), Not(a) & Not(b) & Not(c) & Not(d));
-       		B.set(Not(a) & Not(c) & Not(d),a & Not(d));
-       		C.set(c & Not(d), (b & Not(c) & Not(d)) | (a & Not(c) & Not(d)));
-       		D.set(d,Not(d));
-     	}
+	else{
+		A.set(a & Not(d), Not(a) & Not(b) & Not(c) & Not(d));
+		B.set(Not(a) & Not(c) & Not(d),a & Not(d));
+		C.set(c & Not(d), (b & Not(c) & Not(d)) | (a & Not(c) & Not(d)));
+		D.set(d,Not(d));
+ 	}
 
-     	y -= 29; 
-     	for(int i = 0;i < 30; i++){
-     		circle(x + i, y,0);
-       		delay(10);
-     	}
+	y -= 29; 
+	for(int i = 0;i < 30; i++){
+		circle(x + i, y,0);
+		delay(10);
+ 	}
 
-     	x += 29;
-     	for(int i = 0; i < 30; i++){
-     		circle(x, y+i, 0);
-       		delay(10); 
-     	}
+	x += 29;
+	for(int i = 0; i < 30; i++){
+		circle(x, y+i, 0);
+		delay(10); 
+ 	}
 
-     	y += 29;
-     	for(int i = 0; i < 30; i++){
-     		circle(x + i, y, 0);
-       		delay(10);
-     	}
+	y += 29;
+	for(int i = 0; i < 30; i++){
+		circle(x + i, y, 0);
+		delay(10);
+	}
 
-     	x+=29;
-     	if(x+59>maxx){
-     		x = 10;
-     		y+=100;
-     	}
-    }
- 	return 0;
+	x+=29;
+	if(x+59>maxx){
+		x = 10;
+		y+=100;
+	}
+}
+	return 0;
 }
